1fa0c4bcad183e4fa36375b1036d0fcf46ec0634
GabrielNP96 <gabrielnp1996@gmail.com>
2024-12-20 00:39:32 -0300

    condicionais.

--- curso_screenSound_csharp/Program.cs
+++ curso_screenSound_csharp/Program.cs
@@ -7,3 +7,37 @@ void ExbirMensagemDeBoasVindas()
 }
 
 ExbirMensagemDeBoasVindas();
+
+void ExibirOpcoesDoMenu()
+{
+    Console.WriteLine("\nDigite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("Digite -1 para sair");
+    Console.Write("Digite sua opção: ");
+
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+
+
+    switch (opcaoEscolhidaNumerica) 
+    {
+        case 1 : Console.WriteLine("Você escolheu a opção número 1");
+        break;
+        case 2 : Console.WriteLine("Você escolheu a opção número 2");
+        break;
+        case 3 : Console.WriteLine("Você escolheu a opção número 3");
+        break;
+        case 4 : Console.WriteLine("Você escolheu a opção número 4");
+        break;
+        case -1 : Console.WriteLine("Você escolheu a opção número -1");
+        break;
+        default : Console.WriteLine("invalida");
+        break;
+    }
+
+
+}
+
+ExibirOpcoesDoMenu();
--- curso_screenSound_csharp/obj/Debug/net8.0/curso_screenSound_csharp.AssemblyInfo.cs
+++ curso_screenSound_csharp/obj/Debug/net8.0/curso_screenSound_csharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("curso_screenSound_csharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c01753d7c65b85c087547287774f16eba77a84f")]
 [assembly: System.Reflection.AssemblyProductAttribute("curso_screenSound_csharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("curso_screenSound_csharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
